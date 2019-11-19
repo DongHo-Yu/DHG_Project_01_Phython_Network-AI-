632ec7ab3870ba6754939071a196b2293589ae45
dhy948 <dhy948@gmail.com>
2019-11-20 03:23:29 +0900

    no message

---
+++ Project_Indian/Assets/Test/das.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class das : MonoBehaviour {
+
+    // Use this for initialization
+    private Text tt;
+	void Start () {
+        tt = GetComponent<Text>();
+        tt.text = "굿";	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
